9022a4067c8b89b67ece052dbe43dc6b306705a7
memanlolz <yofungate@gmail.com>
2015-06-08 20:42:25 -0400

    New Ignores.

--- src/com/yofungate/ircBot/login.java
+++ src/com/yofungate/ircBot/login.java
@@ -1,6 +1,10 @@
 package com.yofungate.ircBot;
 
 public class login {
-	public static final String username = "";//username
-	public static final String password = "";//oauth
+	public static final String username = //
+			//"PleaseStopTryingWubs"; //               //
+			"memanlelz";  
+	public static final String password = //
+				//"oauth:lwz0srcqodmhkd4q9vzxht7ulyraup";//
+				"oauth:btd8jwu1sl9jrrft1rccf81zurgqt7";
 }
