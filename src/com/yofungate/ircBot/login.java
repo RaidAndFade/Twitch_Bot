7d5553109c755c9d6ce364ff00836941fce769b2
memanlolz <yofungate@gmail.com>
2015-06-08 20:43:38 -0400

    Update login.java

--- src/com/yofungate/ircBot/login.java
+++ src/com/yofungate/ircBot/login.java
@@ -2,9 +2,5 @@ package com.yofungate.ircBot;
 
 public class login {
 	public static final String username = //
-			//"PleaseStopTryingWubs"; //               //
-			"memanlelz";  
 	public static final String password = //
-				//"oauth:lwz0srcqodmhkd4q9vzxht7ulyraup";//
-				"oauth:btd8jwu1sl9jrrft1rccf81zurgqt7";
 }
